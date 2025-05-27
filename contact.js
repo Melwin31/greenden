e97c3ae43fcb706d6205483377901083710b1b81
Melwin31 <melwin272005@gmail.com>
2025-05-28 00:24:19 +0530

    update product

--- contact.js
+++ contact.js
@@ -8,3 +8,15 @@ menuicon.addEventListener("click",function()
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+  function submitForm(event) {
+  event.preventDefault(); 
+
+  const name = document.querySelector('input[placeholder="Name"]').value;
+
+  alert("Thank you, " + name + "! Your form have been sccessfully registered");
+
+ 
+  document.getElementById("contactForm").reset();
+}
+
+
